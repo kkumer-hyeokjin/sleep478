d9859d7dbf622c1eaaa6c8ff96befaa1687e57fd
kkumer-hyeokjin <qkrgurwls98@naver.com>
2020-09-16 17:11:40 +0900

    sleep478 first commit

--- sleep478/ViewController.swift
+++ sleep478/ViewController.swift
@@ -7,14 +7,41 @@
 //
 
 import UIKit
+import Foundation
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var yellowImage: UIImageView!
+    @IBOutlet weak var blueImage: UIImageView!
+    
+    var loopTimer = Timer()
+    var firstTimer = Timer()
+    var count: Double = 0.1
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+    
     }
-
+    
+    @IBAction func touchStartButton(_ sender: Any) {
+        loopTimer = Timer.scheduledTimer(timeInterval: 19, target: self, selector: #selector(loopTimerAction), userInfo:  nil, repeats: true)
+    }
+    
+    
+    @objc func loopTimerAction() {
+        firstTimer = Timer.scheduledTimer(timeInterval: 0.005, target: self, selector: #selector(firstTimerAction), userInfo: nil, repeats: true)
+    }
+    
+    @objc func firstTimerAction() {
+        if count==140 {
+            firstTimer.invalidate()
+        }
+    }
+    
+    func moveImage(value: Double) {
+        blueImage.transform = CGAffineTransform(translationX: CGFloat(value), y: -CGFloat(value))
+        count+=0.1
+    }
 
 }
 
